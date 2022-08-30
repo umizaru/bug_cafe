d32653de575cd2a8ae6c94d5ac4d3cbee87b026b
umizaru <y.wakita1T@gmail.com>
2022-08-30 23:33:05 +0400

    first commit

--- cafe.rb
+++ cafe.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-
 DRINKS = [
   { name: 'コーヒー', price: '300' },
   { name: 'カフェラテ', price: '400' },
@@ -20,15 +19,15 @@ def take_order(menus)
   end
   print '>'
   order_number = gets.to_i
-  puts "#{menus[order_number][:name]}(#{menus[order_number][:price]}円)ですね。"
+  puts "#{menus[order_number - 1][:name]}(#{menus[order_number - 1][:price]}円)ですね。"
   order_number
 end
 
 puts 'bugカフェへようこそ！ご注文は？ 番号でどうぞ'
-order1 = take_order(DRINKS)
+order_drink = take_order(DRINKS)
 
 puts 'フードメニューはいかがですか?'
-order2 = take_order(FOODS)
+order_food = take_order(FOODS)
 
-total = FOODS[order1][:price] + DRINKS[order2][:price]
+total = DRINKS[order_drink -1][:price].to_i + FOODS[order_food -1][:price].to_i
 puts "お会計は#{total}円になります。ありがとうございました！"
